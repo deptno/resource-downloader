6f842dac071f2debcb661da57d8722e2e09fb3e8
Bonggyun Lee <deptno@gmail.com>
2017-06-05 01:33:46 +0900

    bmp processing

--- src/modules/image-processor/index.ts
+++ src/modules/image-processor/index.ts
@@ -1,5 +1,6 @@
 import * as sharp from 'sharp';
 import * as path from 'path';
+import * as bmp from 'bmp-js';
 
 interface NameStream {
     name: string;
@@ -14,21 +15,30 @@ const splitIfWidthBiggerThenHeight =
 
         try {
             const basename = path.basename(name);
-            const dotExt = path.extname(name);
-            const {width, height} = await metadata(dotExt.slice(1), pipeline);
+            const ext = path.extname(name).slice(1);
             const ret = [];
+            try {
+                const {width, height} = await metadata(ext, pipeline);
 
-            if (width > height) {
-                const half = Math.floor(width / 2);
-                const leftPage = pipeline.clone().extract({left: 0, top: 0, width: half, height});
-                const rightPage = pipeline.clone().extract({left: half, top: 0, width: half, height});
+                if (width > height) {
+                    const half = Math.floor(width / 2);
+                    const leftPage = pipeline.clone().extract({left: 0, top: 0, width: half, height});
+                    const rightPage = pipeline.clone().extract({left: half, top: 0, width: half, height});
 
-                ret.push({name: `${basename}_${l}${dotExt}`, data: leftPage});
-                ret.push({name: `${basename}_${r}${dotExt}`, data: rightPage});
-            } else {
-                ret.push({name, data: pipeline.clone()});
+                    ret.push({name: `${basename}_${l}.${ext}`, data: leftPage});
+                    ret.push({name: `${basename}_${r}.${ext}`, data: rightPage});
+                } else {
+                    ret.push({name, data: pipeline.clone()});
+                }
+            } catch(ex) {
+                const type = supportTypes.find(type => type.format === ext);
+
+                if (!type) {
+                    throw `unsupported type ${ext}`;
+                }
+
+                return splitIfWidthBiggerThenHeight(name, type.handler(pipeline.toBuffer()));
             }
-
             return ret;
         } catch (message) {
             throw new Error(JSON.stringify({name, message}))
@@ -40,23 +50,20 @@ const metadata = async (format: string, pipeline): Promise<Metadata> => {
     try {
         return pipeline.metadata();
     } catch(ex) {
-        const type = supportTypes.find(type => type.format === format);
-
-        if (!type) {
-            throw `unsupported type ${format}`;
-        }
-
-        return type.handler();
+        throw `unsupported type ${format}`;
     }
 };
 
 interface SupportTypes {
     format: string;
-    handler(): Promise<any>;
+    handler(): Promise<NameStream>;
 }
 const supportTypes = [{
     format: 'bmp',
-    handler() {
+    handler(buffer) {
+        const decoded = bmp.decode(buffer);
+        const {width, height, data} = decoded;
 
+        return data;
     }
 }];
